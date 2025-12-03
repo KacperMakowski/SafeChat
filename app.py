5623ee4b01f1349a4bf2603253475eb38749903b
KacperMakowski <kacpermakowski156@gmail.com>
2025-12-03 22:21:18 +0100

    Final version

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY', 'sekretny-klucz-domyslny-do-testow')
+app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
 socketio = SocketIO(app)
 
 users = {}
@@ -89,4 +89,4 @@ def handle_disconnect():
         emit('user_left', nickname, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app)
